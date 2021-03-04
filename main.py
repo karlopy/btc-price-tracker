d21e96f8d05bfa39052510a48fa2b37d753bb453
karlopy <79423960+karlopy@users.noreply.github.com>
2021-03-04 23:01:50 +0700

    Update and rename main to main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from datetime import date
 today = date.today()
 d2 = today.strftime('%B %d, %Y')
 print('BTC Price Tracker')
-print('Made by: karlo#0001')
+print('Made by: karlo#0001') #creds :D
 print(d2)
 yes = True
-while yes == True:
+while yes == True: #loooooop
     data = requests.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD')
     json_data = data.json()
-    price = json_data['USD']
-    time.sleep(90)
+    price = json_data['USD'] #gets USD from the api
+    time.sleep(90) #interval between each check, change number (seconds)
     print(price)
